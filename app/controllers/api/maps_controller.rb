70c1370a89d61eb63f61fcdbfe726e8f2b36cf82
Daiki Nagaoka <neko.z.n1210@gmail.com>
2019-02-24 23:18:26 +0900

    add endpoint

---
+++ app/controllers/api/maps_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  class MapsController < ApplicationController
+    def reverse
+    end
+
+    def autocomplete
+    end
+
+    def direction
+    end
+
+    private
+    
+    def reverse_params
+    end
+
+    def autocomplete_params
+    end
+
+    def direction_params
+    end
+  end
+end
